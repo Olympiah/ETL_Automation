13ff038ca682b67f42a6f70562fbcb29dc1fec15
Olympiah <87874970+Olympiah@users.noreply.github.com>
2025-04-23 11:49:03 +0300

    Complete ETL

---
+++ data_pipeline.py
@@ -0,0 +1,34 @@
+from functions import *
+import time
+import datetime
+
+#printing time pipeline begins to run
+print("Starting data pipeline at", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print('--------------------------------------------------------------------------------')
+
+
+#Extracting the video ID's from videos in Youtube
+t0 = time.time()
+
+getVideoID()
+
+t1 = time.time()
+print("----Video ID's downloaded in", str(t1-t0), "seconds", "\n")
+
+#Extracting the transcripts of each video
+t0 = time.time()
+
+getVideoTranscripts()
+
+t1= time.time()
+print("----Transcripts downloaded----")
+
+# Data Transformation i.e data preprocessing
+transformData()
+print("-----Data Transformation complete!!-----")
+
+#Generate Text Embeddings
+createTextEmbeddings()
+print("----Text Embeddings Generated----")
+
+
